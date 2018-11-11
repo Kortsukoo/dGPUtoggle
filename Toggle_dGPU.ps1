e8d72a93257d8701976c864fc9405a03099569e4
Kortsukoo <44945331+Kortsukoo@users.noreply.github.com>
2018-11-11 16:06:52 +0200

    Add files via upload

---
+++ Toggle_dGPU.ps1
@@ -0,0 +1,17 @@
+﻿# Run as Admin
+
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+} 
+
+
+
+# Checks dGPU state and either enables or disables it.
+
+if (Get-PnpDevice | where {$_.friendlyname -like "NVIDIA*" -and $_.Status -eq "Error"}) {
+Get-PnpDevice| where {$_.friendlyname -like "NVIDIA GeForce GTX 1060 with Max-Q Design*"} | Enable-PnpDevice -Confirm:$false
+}
+else {Get-PnpDevice| where {$_.friendlyname -like "NVIDIA GeForce GTX 1060 with Max-Q Design*"} | Disable-PnpDevice -Confirm:$false}
